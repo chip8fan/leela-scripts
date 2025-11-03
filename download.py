b666bf7338ef88260be15f5031656bf4e179eb29
chip8fan <nguyennamtran2022@gmail.com>
2025-11-03 11:21:11 -0600

    Only include rated games, no casual games

--- download.py
+++ download.py
@@ -2,4 +2,4 @@ import sys
 import webbrowser
 sys.argv.pop(0)
 for user in sys.argv:
-    webbrowser.open(f"https://lichess.org/api/games/user/{user}?tags=true&clocks=false&evals=false&opening=false&literate=false&perfType=blitz%2Crapid%2Cclassical%2Ccorrespondence%2Cstandard")
+    webbrowser.open(f"https://lichess.org/api/games/user/{user}?rated=true&tags=true&clocks=false&evals=false&opening=false&literate=false&perfType=blitz%2Crapid%2Cclassical%2Ccorrespondence%2Cstandard")
