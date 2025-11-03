f6e4241d3ce2cbacf40cc6b7187b33b6e4e6d985
chip8fan <nguyennamtran2022@gmail.com>
2025-11-02 20:27:53 -0600

    Include pgn tags in download

--- download.py
+++ download.py
@@ -1,3 +1,3 @@
 import sys
 import webbrowser
-webbrowser.open(f"https://lichess.org/api/games/user/{sys.argv[1]}?rated=true&tags=false&clocks=false&evals=false&opening=false&literate=false&perfType=ultraBullet%2Cbullet%2Cblitz%2Crapid%2Cclassical%2Ccorrespondence%2Cstandard")
+webbrowser.open(f"https://lichess.org/api/games/user/{sys.argv[1]}?rated=true&tags=true&clocks=false&evals=false&opening=false&literate=false&perfType=ultraBullet%2Cbullet%2Cblitz%2Crapid%2Cclassical%2Ccorrespondence%2Cstandard")
